ac52c57d612424c2a160c0710e65f2935156f2d0
Анжеліка <162304677+milajalika1@users.noreply.github.com>
2024-10-01 16:29:18 +0300

    add

---
+++ src/components/App.jsx
@@ -0,0 +1,15 @@
+// src/components/App.jsx
+
+import Product from "./Product";
+
+function App() {
+	return (
+		<div>
+			<h1>hello</h1>
+
+			<Product />
+		</div>
+	)
+}
+
+export default App
---
+++ src/components/Product.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Product = () => {
+  return (
+      <div>
+          <h3>Profile 1</h3>
+          <p>
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Recusandae, magnam.
+
+          </p>
+    </div>
+  )
+}
+
+export default Product
+
+
